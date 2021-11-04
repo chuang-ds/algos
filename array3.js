511f7140d6a3d29a3ade11cebffa2b5ee923ad88
chuang-ds <chet.huang.vt@gmail.com>
2021-11-03 23:59:44 -0700

    more algos

--- array3.js
+++ array3.js
@@ -32,3 +32,33 @@ function invertHash(arr) {
     return arrReturn;
 }
 console.log(invertHash({"name": "Zaphod", "numHeads": 2}))
+
+// Remove Blanks -- Create a function that, given a string, returns the string, without blanks. Given ​" play that Funky Music "​, returns a string containing ​"playthatFunkyMusic"​.
+function removeSpaces(str) {
+    myArr = str.split(" ");
+    newStr = myArr.join("");
+    return newStr;
+}
+console.log(removeSpaces(" play that Funky Music "));
+
+// Get String Digits -- Create a JavaScript function that given a string, returns the integer made from the string’s digits. Given "0s1a3y5w7h9a2t4?6!8?0"​, the function should return the number 1,357,924,680.
+function getStringDigits(str) {
+    let newStr = "";
+    for(let i=0; i<str.length; i++) {
+        if(str[i] >= 0 || str[i] <= 9) {
+            newStr += str[i];
+        }
+    }
+    return parseInt(newStr);
+}
+console.log(getStringDigits("0s1a3y5w7h9a2t4?6!8?0"));
+
+// Acronyms -- Create a function that, given a string, returns the string’s acronym (first letters only, capitalized). Given "there's no free lunch - gotta pay yer way"​, return ​"TNFL-GPYW"​. Given ​"Live from New York, it's Saturday Night!"​, return ​"LFNYISN"​.
+function acronyms(str) {
+    myArr = str.split(" ");
+    // for(let i=0; i<myArr.length; i++) {
+    //     myArr[i] = myArr[i][0].toUpperCase();
+    // }
+    return myArr.join("");
+}
+console.log(acronyms("test"​));
--- fundamentals6.js
+++ fundamentals6.js
@@ -60,3 +60,44 @@ generateChange(140);
 generateChange(87);
 generateChange(25);
 generateChange(3);
+
+// Messy Math Mashup -- Create a function messyMath(num) that will return the following sum: add all integers from 0 up to the given num, except for the following special cases of our count value:
+// If current count (not num) is evenly divisible by 3, don’t add to the sum; skip to the next count;
+// Otherwise, if the current count is evenly divisible by 7, include it twice in sum instead of once;
+// Regardless of the above, if the current count is exactly 1/3 of num, return -1 immediately.
+// For example, if given num is 4, return 7. If given num is 8, return 34. If given num is 15, return -1.
+function messyMath(num) {
+    let sum = 0;
+    for(let i = 0; i <= num; i++) {
+        if((num/3) == i) {
+            return -1;
+        }
+        if(i % 3 == 0) {
+            continue;
+        }
+        else if(i % 7 == 0) {
+            sum += i*2;
+        }
+        else {
+            sum += i;
+        }
+    }
+    return sum;
+}
+console.log(messyMath(4));
+console.log(messyMath(8));
+console.log(messyMath(15));
+
+//Twelve-Bar Blues -- Write a function that console.logs the number 1, then "chick", then "boom", then "chick", then 2, then "chick", "boom", "chick" – continuing the same cycle for each number up to (including) 12.
+function twelveBarBlues() {
+    for(let i = 1; i <= 12; i++) {
+        console.log(i);
+        console.log("chick");
+        console.log("boom");
+        console.log("chick");
+    }
+}
+twelveBarBlues();
+
+// Fibonacci -- Create a function to generate Fibonacci numbers. In this famous mathematical sequence, each number is the sum of the previous two, starting with values 0 and 1. Your function should accept one argument, an index into the sequence (where 0 corresponds to the initial value, 4 corresponds to the value four later, etc). 
+function fibonacci()
