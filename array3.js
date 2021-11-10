80c2fae302baec638f91b8a0393d477bf151ac8f
chuang-ds <chet.huang.vt@gmail.com>
2021-11-10 00:11:54 -0800

    arrays

--- array3.js
+++ array3.js
@@ -101,3 +101,37 @@ let moveZeroLeft = function(arr) {
 let a = [1,10,20,0,59,63,0,88,0];
 moveZeroLeft(a);
 console.log(a);
+
+// Merge Overlapping Intervals - You are given an array (list) of interval pairs as input where each interval has a start and end timestamp. The input array is sorted by starting timestamps. You are required to merge overlapping intervals and return a new output array.  Consider the input array below. Intervals (1, 5), (3, 7), (4, 6), (6, 8) are overlapping so they should be merged to one big interval (1, 8). Similarly, intervals (10, 12) and (12, 15) are also overlapping and should be merged to (10, 15).
+class Pair { 
+    constructor(first, second) { 
+      this.first = first; 
+      this.second = second; 
+    } 
+  }
+  
+  let mergeIntervals = function(v){
+    let result = [];
+    result.push(new Pair(v[0].first, v[0].second));
+
+    for(let i = 0; i < v.length; i++) {
+        let currInterval = [new Pair(v[i].first, v[i].second)];
+        let currX = v[i].first;
+        let currY = v[i].second;
+        let resultX = result[result.length -1].first;
+        let resultY = result[result.length -1].second;
+        if (resultY >= currX) {
+            result[result.length -1].second = Math.max(currY, resultY);
+        }
+        else {
+            result.push(new Pair(currX, currY));
+        }        
+    }
+    return result;    
+  }
+let a=[new Pair(1, 5), new Pair(3, 7), new Pair(4, 6), new Pair(6, 8), new Pair(10, 12), new Pair(11, 15)];
+let result = mergeIntervals(a);
+console.log(result.length);
+for (let i=0; i < result.length; i++) {
+    console.log(result[i].first + ", " + result[i].second);
+}
