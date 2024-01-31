92ea93c5cda72c4706ade658cef588801ff58203
Chet Huang <chet.huang.vt@gmail.com>
2024-01-30 23:40:39 -0800

    tournamentWinner

--- tournamentWinner.js
+++ tournamentWinner.js
@@ -1,30 +1,10 @@
 /*
-There's an algo tournament taking place in which teams of programmers compete against each other to solve
-algo problems as fast as possible.  Teams compete in a round robin, where each team faces off against
-all other teams.  Only two teams compete against each other at a time, and for each competition, one team
-is designated the home team, while the other team is the away team.  In each competition there's always
-one winner and one loser; there are no ties.  A team receives 3 points if it wins and 0 points if it
-loses.  The winner of the tournament is the team that receives the most amount of points.
-
-Given an array of pairs representing the teams that have completed against each other and an array
-containing the results of each competition, write a function that returns the winner of the tournament.
-The input arrays are named competitions and results, respectively.  The competitions array has elements
-in the form of [homeTeam, awayTeam], where each team is a string of at most 30 characters representing
-the name of the team.  The results array contains information about the winner of each corresponding
-competition in the competitions array.  Specifically, results[i] denotes the winner of competitions[i],
-where 1 in the reults array means that the home team in the corresponding compeition won and a 0 means
-the away team won.
-
-It's guaranteed that exactly one team will win the tournament and that each team will compete against
-all the other teams exactly once.  It's also guaranteed that the tournament will always have at least
-two teams.
+tournament winner.  array named competitions contains names of home team, visiting team.
+array named results contains 0 if home team won, 1 if visiting team won.  Each win counts as 3 pts.
+Determine winning team.  index in each array represents match between teams and results
 
 Sample input:
- competitions = [
-    ["HTML", "C#"],
-    ["C#", "Python"],
-    [Python", "HTML]"],
- ]
+ competitions = [ ["HTML", "C#"], ["C#", "Python"], ["Python", "HTML"] ]
  results = [0, 0, 1]
 
  Sample output:
