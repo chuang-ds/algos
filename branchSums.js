02a677e2991c06f9500b7833a9bc86819148b043
Chet Huang <chet.huang.vt@gmail.com>
2024-02-11 21:09:11 -0800

    branchSums finished using recursion

--- branchSums.js
+++ branchSums.js
@@ -31,13 +31,26 @@ class BinaryTree {
     }
   }
   
-  function branchSums(root) {
-    currentSum = 0;
-    currentNode = root;
-    while (currentNode != null) {
-        currentSum += currentNode.value;
-    }
-  }
-  
+  function branchSums(root, recursionSum = 0, results = []) {
 
-  
+    // keep tally or currentSum of current root node value plus sum passed in from recurrsion
+    currentSum = root.value + recursionSum;
+
+    // if not leaf then end of node so add currentSum to results array
+    if (!root.left && !root.right) {
+      results.push(currentSum);
+    }
+
+    // if there is leaf left then call recursive function passing in left node, currentSum, and results array
+    if (root.left) {
+      branchSums(root.left, currentSum, results);
+    }
+
+    // if there is leaf right then call recursive function passing in right node, currentSum, and results array
+    if (rootright) {
+      branchSums(root.right, currentSum, results);
+    }
+
+    // once all leaf nodes exausted then return final results array
+    return results;
+  }
